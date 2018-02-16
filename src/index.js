ed3cead7462541393d0fc17f0bb0130e3cff52e0
Joe <joe.ekiert@gmail.com>
2018-02-15 21:31:07 -0800

    moving registerServiceWorker into utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Resume from './components/Resume';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(<Resume />, document.getElementById('root'));
 registerServiceWorker();
