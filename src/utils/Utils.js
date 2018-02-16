e0181603cc2b228b6e24667a49c4c05230ec53c6
Joe <joe.ekiert@gmail.com>
2018-02-15 21:32:04 -0800

    little namey changey

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,6 +1,6 @@
-const util = {};
+const Utils = {};
 
-util.uuidv4 = () => {
+Utils.uuidv4 = () => {
   // https://stackoverflow.com/a/2117523/355627
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -8,4 +8,4 @@ util.uuidv4 = () => {
   });
 }
 
-export default util;
+export default Utils;
