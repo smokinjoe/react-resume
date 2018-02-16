6a6c34ef9c9db7ae465ee169e432e339786c40d2
Joe <joe.ekiert@gmail.com>
2018-02-15 21:31:14 -0800

    new uuidv4 util method

---
+++ src/utils/util.js
@@ -0,0 +1,11 @@
+const util = {};
+
+util.uuidv4 = () => {
+  // https://stackoverflow.com/a/2117523/355627
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+export default util;
