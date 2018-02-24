2679469b8ef19fd0310845b35af9d6cd93ff629b
Joe <joe.ekiert@gmail.com>
2018-02-23 16:49:39 -0800

    storing user data in redux!

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,13 @@
+/* eslint-disable import/first */
+// JOE: the lame way I deal with reducers is that I like having reducer
+// blocks, which causes all sorts of parse errors since I have imports
+// all over the place
+
 import { combineReducers } from 'redux';
+
+/**
+* Local .json file LOAD action
+*/
 
 import {
   LOAD_RESUME
@@ -20,8 +29,43 @@ const resume = (state = initialResumeState, action) => {
   }
 };
 
+/**
+* Load user data from API endpoint
+*/
+
+import {
+  LOAD_USER_DATA
+} from '../actions';
+
+const initialUserData = {
+  city: null,
+  email: null,
+  name: null,
+  phone: null,
+  state: null,
+  street_address: null,
+  website: null,
+  zip: null
+};
+
+const user = (state = initialUserData, action) => {
+  switch (action.type) {
+    case LOAD_USER_DATA:
+      return Object.assign({}, state, action.data);
+      break;
+
+    default:
+      return state;
+  }
+};
+
+/**
+* Combine 'em all
+*/
+
 const reducers = {
-  resume
+  resume,
+  user
 };
 
 const rootReducer = combineReducers(reducers);
