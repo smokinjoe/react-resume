ebc23a57fccda23eb27efefbc91e9b5f5c7dc2d7
Joe <joe.ekiert@gmail.com>
2018-02-23 16:49:30 -0800

    hiding secret key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
-export const LOAD_RESUME = 'LOAD_RESUME';
 
+/**
+* Load resume data from local json file
+*/
+
+export const LOAD_RESUME = 'LOAD_RESUME';
 export const loadResume = () => (dispatch) => _loadResume(dispatch);
 
 const _loadResume = (dispatch) => {
@@ -9,4 +13,30 @@ const _loadResume = (dispatch) => {
     type: LOAD_RESUME,
     data: json
   });
+};
+
+/**
+* Load user data from API endpoint
+*/
+
+export const LOAD_USER_DATA = 'LOAD_USER_DATA';
+export const loadUserData = () => (dispatch) => _loadUserData(dispatch);
+
+const _loadUserData = (dispatch) => {
+  fetch('http://localhost:8080/v1/users?api_key=6BYfKXXVP3gjt9wT9RybkE', {
+    method: 'GET',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': 'Bearer ' + process.env.REACT_APP_JOE_RESUME_API_SECRET,
+      'Content-Type': 'Content-Type: application/json'
+    }
+  }).then(response => response.json())
+  .then(items => {
+    if (typeof items.payload !== 'undefined') {
+      dispatch({
+        type: LOAD_USER_DATA,
+        data: items.payload.pop()
+      });
+    }
+  })
 };
