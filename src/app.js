8f03b7def270d04e21aad3ab984ff2202da2567a
Joe <joe.ekiert@gmail.com>
2018-03-02 15:44:13 -0800

    adding a new wrapper layer

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Resume from './components/Resume';
+
+class App extends Component {
+  render () {
+    return <Resume />
+  }
+};
+
+export default App;
