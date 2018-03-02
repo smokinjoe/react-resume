80fa061ce5bd2b62bb14e9eacd226e46df6c25d2
Joe <joe.ekiert@gmail.com>
2018-03-02 15:55:48 -0800

    new middle layer to deal with routing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+
+import { Switch, Route, Link } from 'react-router-dom';
+
 import Resume from './components/Resume';
 
 class App extends Component {
   render () {
-    return <Resume />
+    return (
+      <div>
+        { /* <Header /> */ }
+        <Main />
+      </div>
+    );
   }
 };
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Root</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={ Resume } />
+    <Route path='/login' component={ Login } />
+  </Switch>
+);
+
+const Login = () => (
+  <h1>Login</h1>
+);
 
 export default App;
