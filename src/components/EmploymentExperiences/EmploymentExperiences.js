25f34e4a31c5f6d6a0c65f0ca20bf54688838e9b
Joe <joe.ekiert@gmail.com>
2018-03-20 15:41:37 -0700

    WIP: trying to update items onChange

--- src/components/EmploymentExperiences/EmploymentExperiences.js
+++ src/components/EmploymentExperiences/EmploymentExperiences.js
@@ -42,38 +42,92 @@ class EmploymentExperiences extends Component {
 
   renderEditToggleFor (id) {
     let canEdit = this.props.edit;
+    let { editing, editingId } = this.state;
 
-    if (canEdit) {
+    if (canEdit && (!editing || editingId === id)) {
       return (
         <button onClick={ this.toggleEditingFor.bind(this, id) }>Edit</button>
       );
     }
   }
 
+  handleChange (i, event) {
+    console.log('JOE: i: ', i);
+    console.log('JOE: event.target.value: ', event.target.value);
+
+    let { experience } = this.state;
+
+    experience.items.forEach((obj, _i) => {
+      if (_i === i) {
+        obj = event.target.value
+      }
+    });
+
+    this.setState({ experience });
+  }
+
+  handleSubmit () {
+    console.log('JOE: this.state.experience: ', this.state.experience);
+  }
+
   renderEmploymentExperiences () {
     let arr = [];
+    let { experience, editingId } = this.state;
 
     this.props.data.forEach((xp) => {
-      let tmpArray = [];
-      xp.items.forEach((r, i) => {
-        tmpArray.push(
-          <li key={ i }>
-            { r }
-          </li>
-        );
-      });
 
-      arr.push(
-        <div key={ xp.id }>
-          <h3>{ xp.company_name } { this.renderEditToggleFor(xp.id) }</h3>
-          <div className={['subheader']}>
-            { xp.date_start } ~ { xp.date_end }
+      if (editingId === xp.id) {
+
+        let tmpArray = [];
+        experience.items.forEach((r, i) => {
+          tmpArray.push(
+            <div key={ i }>
+              <input
+                  type='text'
+                  value={ r }
+                  onChange={ this.handleChange.bind(this, i) } />
+              <button onClick={ this.handleSubmit.bind(this) }>Save</button>
+            </div>
+          );
+        });
+
+        arr.push(
+          <div key={ xp.id }>
+            <h3>{ xp.company_name } { this.renderEditToggleFor(xp.id) }</h3>
+            <div className={['subheader']}>
+              { xp.date_start } ~ { xp.date_end }
+            </div>
+            <ul className={['experience']}>
+              { tmpArray }
+            </ul>
           </div>
-          <ul className={['experience']}>
-            { tmpArray }
-          </ul>
-        </div>
-      );
+        );
+
+      }
+      else {
+
+        let tmpArray = [];
+        xp.items.forEach((r, i) => {
+          tmpArray.push(
+            <li key={ i }>
+              { r }
+            </li>
+          );
+        });
+
+        arr.push(
+          <div key={ xp.id }>
+            <h3>{ xp.company_name } { this.renderEditToggleFor(xp.id) }</h3>
+            <div className={['subheader']}>
+              { xp.date_start } ~ { xp.date_end }
+            </div>
+            <ul className={['experience']}>
+              { tmpArray }
+            </ul>
+          </div>
+        );
+
+      }
 
     });
 
